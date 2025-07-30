caddc61c15ab493fb09d7216959050551dd2d40b
Peter-Ross Cuthbert <peter-ross@greenlightsolutions.co.za>
2025-07-30 10:55:57 +0200

    Quartz sync: Jul 30, 2025, 10:55 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "redrockvp.github.io/quartztesting",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
